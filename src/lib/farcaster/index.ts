afac79914cf3fd1010df1d0138177e302d72c0e9
shixuewen <friendlysxw@163.com>
2025-06-07 21:19:03 +0800

    feat: open farcaster

--- src/lib/farcaster/index.ts
+++ src/lib/farcaster/index.ts
@@ -31,15 +31,14 @@ export const openFarcasterCreateCast = async ({
 
   if (isMobile) {
     // 移动端优先尝试打开 app，延迟后跳 web
-    // const timeout = setTimeout(() => {
-    //   window.open(webUrl, "_blank");
-    // }, 1500);
-    // window.location.href = farcasterAppUrl;
-    // // 用户手动切回浏览器时清除定时器
-    // window.addEventListener("pagehide", () => clearTimeout(timeout), {
-    //   once: true,
-    // });
-    window.open(farcasterAppUrl, "_blank");
+    const timeout = setTimeout(() => {
+      window.open(webUrl, "_blank");
+    }, 1500);
+    window.location.href = farcasterAppUrl;
+    // 用户手动切回浏览器时清除定时器
+    window.addEventListener("pagehide", () => clearTimeout(timeout), {
+      once: true,
+    });
   } else {
     // PC 端直接打开 web
     window.open(webUrl, "_blank");
